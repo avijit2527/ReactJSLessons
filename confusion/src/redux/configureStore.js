8eb312a0c3e4f4bf93897f39d114653ea98c9182
avijit2527 <iamavijitroy@gmail.com>
2020-05-26 19:33:10 +0530

    combining reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,10 +1,22 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+import { Comments } from './comments';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+
+        })
     );
 
     return store;
